4bfc90e215ffccafae7e84a12e37bd1cc30fc501
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-09-25 00:36:43 +0200

    changes

---
+++ CLI/UI/ManagePosts/ManagePostsView.cs
@@ -0,0 +1,68 @@
+﻿using RepositoryContracts;
+
+namespace CLI.UI.ManagePosts;
+
+public class ManagePostsView
+{
+    private readonly IPostRepository postRepository;
+    private readonly ICommentRepository commentRepository;
+    private readonly IUserRepository userRepository;
+
+    public ManagePostsView(IPostRepository postRepository, ICommentRepository commentRepository, IUserRepository userRepository)
+    {
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+    }
+
+    public async Task ShowAsync()
+    {
+        Console.WriteLine();
+        while (true)
+        {
+            PrintOptions();
+            string input = Console.ReadLine() ?? "";
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Please select an option.\n\n");
+                continue;
+            }
+
+            if ("<".Equals(input))
+            {
+                return;
+            }
+
+            switch (input)
+            {
+                case "1":
+                    CreatePostView createPostView = new CreatePostView(postRepository, userRepository);
+                    await createPostView.ShowAsync();
+                    break;
+                case "4":
+                    ListPostsView listPostsView =
+                        new ListPostsView(postRepository, commentRepository, userRepository);
+                    await listPostsView.ShowAsync();
+                    break;
+                case "<": return;
+                default:
+                    Console.WriteLine("Invalid option, please try again.\n\n");
+                    break;
+            }
+        }
+    }
+
+    private static void PrintOptions()
+    {
+        Console.WriteLine();
+        const string menuOptions = """
+                                   Please select:
+                                   1) Create new post
+                                   2) Update post
+                                   3) Delete post
+                                   4) View posts
+                                   <) Back
+                                   """;
+        Console.WriteLine(menuOptions);
+    }
+}
---
+++ CLI/UI/ManageUsers/ListUsersView.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI.ManageUsers;
+
+public class ListUsersView
+{
+    private readonly IUserRepository userRepository;
+
+    public ListUsersView(IUserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine();
+         ViewUsersAsync();
+    }
+    
+    private void ViewUsersAsync()
+    {
+        IEnumerable<User> manyAsync = userRepository.GetMany();
+        List<User> users = manyAsync.OrderBy(u => u.Id).ToList();
+        
+        Console.WriteLine("Users:");
+        Console.WriteLine("[");
+        foreach (User user in users)
+        {
+            Console.WriteLine($"\tID: {user.Id}, Name: {user.UserName}");
+        }
+
+        Console.WriteLine("]");
+        Console.WriteLine();
+    }
+}
