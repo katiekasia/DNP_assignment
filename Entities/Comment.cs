5a3bae2133fcb6e9709833439de8b0e4aa0d06d9
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-10-27 17:12:15 +0100

    Assignment 4 working full

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -4,8 +4,8 @@ public class Comment
 {
     public int Id { get; set; }
     public string Body { get; set; } // This has a setter, because a comment can be edited after creation.
-    public int PostId { get; } // this property has no setter, because it does not make sense to modify it after creation.
-    public int UserId { get; }
+    public int PostId { get; set; } // this property has no setter, because it does not make sense to modify it after creation.
+    public int UserId { get; set; }
 
     // This constructor is not strictly necessary.
     // You could instead instantiate a Comment using object initializer.
--- Server/WebAPI/Controllers/CommentsController.cs
+++ Server/WebAPI/Controllers/CommentsController.cs
@@ -1,3 +1,5 @@
+using ApiContracts;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
 using RepositoryContracts;
 
@@ -13,4 +15,57 @@ public class CommentsController : ControllerBase
     {
         this.commentRepo = commentRepo;
     }
+
+    [HttpPost]
+    public async Task<IResult> AddComment([FromBody] CreateCommentDto request,
+        [FromServices] ICommentRepository commentRepo)
+    {
+        Comment comment =
+            new Comment(request.Body, request.PostId, request.UserId);
+        Comment created = await commentRepo.AddAsync(comment);
+        CreateCommentDto dto = new()
+        {
+            Body = created.Body,
+            PostId = created.PostId,
+            UserId = created.UserId
+        };
+        return Results.Created($"/api/comment/{created.Id}", dto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IResult> ReplaceComment([FromRoute] int id,
+        [FromBody] CreateCommentDto request,
+        [FromServices] ICommentRepository commentRepo)
+    {
+        Comment existing = await this.commentRepo.GetSingleAsync(id);
+        existing.Body = request.Body;
+        existing.PostId = request.PostId;
+        existing.UserId = request.UserId;
+        await this.commentRepo.UpdateAsync(existing);
+        return Results.Ok();
+
+    }
+
+    [HttpGet("user/{id}")]
+    public async Task<IResult> GetCommentsByUserId([FromRoute] int id)
+    {
+        var comments = commentRepo.GetMany();
+        comments= comments.Where(c => c.UserId == id);
+        return Results.Ok();
+    }
+    [HttpGet("post/{postId}")]
+    public async Task<IResult> GetCommentsByPostId([FromRoute] int postId)
+    {
+        var comments = commentRepo.GetMany();
+        comments = comments.Where(c => c.PostId == postId);
+        return Results.Ok(comments);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IResult> DeleteComment([FromRoute] int id)
+    {
+        await commentRepo.DeleteAsync(id);
+        return Results.NoContent();
+    }
+     
 }
